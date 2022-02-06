aa44d30bba0cc91fed52c8188711a2390d5d8fcc
suptalentdev <suptalentdev@gmail.com>
2022-02-06 19:22:42 +0100

    cleanup error handling

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import 'dotenv/config'
 
-const mongoURI = process.env.MONGOOSE_URI
+const mongoURI = process.env.MONGOOSE_URI || 'mongodb://127.0.0.1:27017/snippets'
 try {
 	const db = await mongoose.connect(mongoURI)
 	console.log(db.connections[0].name)
