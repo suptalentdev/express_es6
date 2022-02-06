60b8dae8ef4d001395a78bd37380f2cec45f6217
suptalentdev <suptalentdev@gmail.com>
2022-02-06 19:39:04 +0100

    add public folder

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 import 'dotenv/config'
 
-const mongoURI = process.env.MONGOOSE_URI || 'mongodb://127.0.0.1:27017/snippets'
+const mongoURI = process.env.MONGOOSE_URI || 'mongodb://127.0.0.1:27017/express-boilerplate'
 try {
 	const db = await mongoose.connect(mongoURI)
-	console.log(db.connections[0].name)
+	console.log(`Connected to database ${db.connections[0].name}`)
 } catch (err) {
 	console.log(`Error while creating connection ${err}`)
 }
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import Snippet from '../models/Snippet.js'
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
